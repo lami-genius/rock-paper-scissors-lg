d382fd99a4d9523d0624fab6c91c63c595ed4ead
lami-genius <fonyuyborislami@gmail.com>
2021-11-29 19:38:33 +0200

    Completed responsive landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
+  // attributes
+  private _rockImgLink: string = './assets/img/real-rock.png'
+  private _paperImgLink: string = './assets/img/real-paper.png'
+  private _scissorsImgLink: string = './assets/img/real-scissors.png'
+  private _playImgLink: string = './assets/img/play-button.png'
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // getters and setters
+
+  public get rockImgLink(): string {
+    return this._rockImgLink;
+  }
+
+  public set rockImgLink(rockImgLink: string) {
+    this._rockImgLink = rockImgLink;
+  }
+
+  public get paperImgLink(): string {
+    return this._paperImgLink;
+  }
+
+  public set paperImgLink(paperImgLink: string) {
+    this._paperImgLink = paperImgLink;
+  }
+
+  public get scissorsImgLink(): string {
+    return this._scissorsImgLink;
+  }
+
+  public set scissorsImgLink(scissorsImgLink: string) {
+    this._scissorsImgLink = scissorsImgLink;
+  }
+
+  public get playImgLink(): string {
+    return this._playImgLink;
+  }
+
+  public set playImgLink(playImgLink: string) {
+    this._playImgLink = playImgLink;
+  }
+
 
 }
