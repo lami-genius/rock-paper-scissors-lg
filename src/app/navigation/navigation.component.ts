8b00fecc9d66c650b82d00e5bdda2cc6f68631bb
lami-genius <fonyuyborislami@gmail.com>
2021-11-29 07:16:40 +0200

    Navigation design completed

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'rpsLG-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
+
 export class NavigationComponent implements OnInit {
+  private _logoImgUrl: string = "./assets/img/logo.png"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // getters and setters
+  public get logoImgUrl(): string {
+    return this._logoImgUrl;
+  }
+
+  public set logoImgUrl(logoImgUrl: string) {
+    this._logoImgUrl = logoImgUrl;
   }
 
 }
